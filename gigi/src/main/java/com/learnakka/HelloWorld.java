8d926700946729bd9904426d1178c2f1930703fa
Michiel Cuijpers <m.cuijpers@sig.eu>
2018-05-22 15:05:43 +0200

    om ticket #175 op te lossen

--- gigi/src/main/java/com/learnakka/HelloWorld.java
+++ gigi/src/main/java/com/learnakka/HelloWorld.java
@@ -14,6 +14,30 @@ public class HelloWorld {
 		int third = first+second;
 		int third = first+second;
 		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
 		
 		return third;
 	}
